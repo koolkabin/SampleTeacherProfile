d8462a310af0442f84fa0a692a8092ab596b57f4
Kabindra Bakey <koolkabin@live.com>
2023-08-22 11:18:28 +0545

    todo

---
+++ CollegeWebsiteAdmin/Controllers/TodoAppController.cs
@@ -0,0 +1,60 @@
+﻿using CollegeWebsiteAdmin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeWebsiteAdmin.Controllers
+{
+    public class TodoAppController : Controller
+    {
+        private readonly MyDBContext _context;
+        public TodoAppController(MyDBContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AjaxAdd(string SubjectName)
+        {
+            try
+            {
+                //code for saving data to database
+                Subject s1 = new Subject();
+                s1.SubjectName = SubjectName;
+
+                _context.Add(s1);
+
+                _context.SaveChanges();
+                return Json(new { status = true });
+
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { status = false, err = ex.Message });
+            }
+
+
+        }
+
+
+        public IActionResult AjaxList()
+        {
+            try
+            {
+                IList<Subject> DataList = _context.Subjects.ToList();
+                return Json(DataList);
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status = false, err = ex.Message });
+            }
+
+
+        }
+    }
+}
